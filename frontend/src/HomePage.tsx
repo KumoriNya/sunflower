324812a4d5ea92a2a0eb2a48e0c8bbdcb8b0569a
Abigail <z5314102@ad.unsw.edu.au>
2023-12-30 17:36:56 +1100

    Feature: Prototype of live caption display done. Implementation: HomePage connects with the port of the server. Handles message display and removal of early message. TODO: Include handles for connection breaks, such as attempts to reconnect, and message from the server to indicate whether the event is finished or not

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,10 +1,54 @@
 import React from "react";
-// import { useNavigate, Outlet, useLocation } from "react-router";
-// import { Box, Typography, Button, createTheme, ThemeProvider, Breadcrumbs } from "@mui/material";
-// import Menubar from "../components/menubar";
+import Websocket from "websocket";
 
 function HomePage () {
-    return (<>Hey?</>)
+    const [socket, setSocket] = React.useState<null|WebSocket>();
+    const [content, setContent] = React.useState<string>("");
+
+    React.useEffect(() => {
+        // TOOD: find an unused socket to replace 8000
+        const ws = new WebSocket('ws://192.168.1.2:8000')
+        ws.onopen = () => {
+            console.log('Connection opened')
+        }
+
+        ws.onmessage = (event) => {
+            const message = event.data
+            // console.log(`Message received: ${message}, type = ${typeof(message)}, content is ${content}`)
+            setContent((prevContent) => {
+                const updatedContent = prevContent + message;
+                const words = updatedContent.split(" ");
+        
+                if (words.length > 120) {
+                  words.shift();
+                  const croppedContent = words.join(" ");
+                  return croppedContent;
+                }
+        
+                return updatedContent;
+            });
+            // handle what to do with the msg
+        }
+
+        ws.onclose = () => {
+            console.log('Connection closed')
+        }
+
+        setSocket(ws);
+
+        return () => {
+            ws.close();
+        };
+    }, [])
+
+    return (
+    <>
+        {/* Container for message */}
+        Live Caption:
+        <div>
+            <p>{content}</p>
+        </div>
+    </>)
 }
 
 export default HomePage
